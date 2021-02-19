73775fc94b0d93d5d05bb55425b0db6a87876257
jeop1977 <juanestebanochoa@gmail.com>
2021-02-19 00:04:50 -0300

    Code without Eval

--- calculator.js
+++ calculator.js
@@ -2,16 +2,60 @@ function operacion(){
     
     let valor = (document.getElementById("numero")).value;
     
-    let operador = valor.indexOf("+");
-
-    let num_1 = valor.substring(0, operador);
-    let num_2 = valor.substring(operador+1);
-
-    let num1 = Number(num_1);
-    let num2 = Number(num_2);
     
-    let res = num1+num2;
-    console.log(res);
+    if (valor.indexOf("+") != -1){
 
-    solucion.innerHTML = res;
+        let operador = valor.indexOf("+");
+        let num_1 = valor.substring(0, operador);
+        let num_2 = valor.substring(operador+1);
+
+        let num1 = Number(num_1);
+        let num2 = Number(num_2);
+        
+        let res = num1+num2;
+        console.log(res);
+
+        solucion.innerHTML = res;
+    } else if(valor.indexOf("-") != -1){
+
+        let operador = valor.indexOf("-");
+        let num_1 = valor.substring(0, operador);
+        let num_2 = valor.substring(operador+1);
+
+        let num1 = Number(num_1);
+        let num2 = Number(num_2);
+
+        let res = num1-num2;
+        console.log(res);
+
+        solucion.innerHTML = res;
+    } else if(valor.indexOf("*") != -1){
+
+        let operador = valor.indexOf("*");
+        let num_1 = valor.substring(0, operador);
+        let num_2 = valor.substring(operador+1);
+
+        let num1 = Number(num_1);
+        let num2 = Number(num_2);
+
+        let res = num1*num2;
+        console.log(res);
+
+        solucion.innerHTML = res;
+    } else if(valor.indexOf("/") != -1){
+
+        let operador = valor.indexOf("/");
+        let num_1 = valor.substring(0, operador);
+        let num_2 = valor.substring(operador+1);
+
+        let num1 = Number(num_1);
+        let num2 = Number(num_2);
+
+        let res = num1/num2;
+        console.log(res);
+
+        solucion.innerHTML = res;
+    } else {
+        solucion.innerHTML = "ERROR!!!";
+    }
 }
